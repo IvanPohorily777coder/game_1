0dd445ae35ac9ca3d275e96415aa66187b1eb511
IvanPohorily777coder <78176713+IvanPohorily777coder@users.noreply.github.com>
2022-03-21 14:32:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let guesses = document.querySelector('.guesses');
+let lastResult = document.querySelector('.lastResult');
+let lowOrHi = document.querySelector('.lowOrHi');
+
+let guessSubmit = document.querySelector('.guessSubmit');
+let guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);     
+    if (guessCount === 1) {
+        guesses.textContent = 'Предыдущие ответы: ';
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Правильно.';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+        
+    } else if (guessCount === 10) {
+        lastResult.textContent = 'Неправильно';
+        setGameOver();
+
+    } else {
+        lastResult.textContent = 'Wrong';
+        lastResult.style.backgroundColor = 'red';
+
+        if(userGuess < randomNumber) {
+            lowOrHi.textContent = 'Мало';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Много';
+        }
+    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Начать новую игру';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    let resetParas = document.querySelectorAll('.resultParas p');
+    for(let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
